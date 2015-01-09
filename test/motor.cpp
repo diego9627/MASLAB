0edb3fc785d6019fe5fe5a6b432f67cd4531367f
Diego Roque <diego9627@gmail.com>
2015-01-09 17:08:24 -0500

    vision

---
+++ test/motor.cpp
@@ -0,0 +1,101 @@
+// Compile with:
+// g++ motor.cpp -o motor -lmraa
+// Controls a motor through a range of speeds using the Cytron motor controller
+// Pwm on pin 9, and dir on pin 8.
+
+//new added comment
+
+#include <cassert>
+#include <cmath>
+#include <csignal>
+#include <iostream>
+
+#include "mraa.hpp"
+
+int running = 1;
+
+void setMotorSpeed(mraa::Pwm& pwm, mraa::Gpio& dir, double speed) {
+  assert(-1.0 <= speed && speed <= 1.0);
+  if (speed < 0) {
+    dir.write(1);
+  }
+  else {
+    dir.write(0);
+  }
+  pwm.write(fabs(speed));
+}
+
+//these two functions just explicitly set the motor forward or backward
+//they are redundant and might not be useful
+//take them out if not needed
+void forward(mraa::Pwm& pwm, mraa::Gpio& dir, double speed) {
+  assert(speed >= 0.0 && speed <= 1.0);
+  dir.write(0);
+  pwm.write(speed);
+}
+
+void reverse(mraa::Pwm& pwm, mraa::Gpio& dir, double speed) {
+  assert(speed >= 0.0 && speed <= 1.0);
+  dir.write(1);
+  pwm.write(speed);
+}
+
+void stop(mraa::Pwm& pwm, mraa::Gpio& dir) {
+  pwm.write(0);
+  dir.write(0);
+}
+
+void sig_handler(int signo)
+{
+  if (signo == SIGINT) {
+    printf("closing spi nicely\n");
+    running = 0;
+  }
+}
+
+int main() {
+  // Handle Ctrl-C quit
+  signal(SIGINT, sig_handler);
+  
+  //two motor setup
+  mraa::Pwm left_motor = mraa::Pwm(9);
+  left_motor.write(0.0);
+  left_motor.enable(true);
+  mraa::Gpio left_dir = mraa::Gpio(8);
+  left_dir.dir(mraa::DIR_OUT);
+  left_dir.write(0);
+
+  mraa::Pwm right_motor = mraa::Pwm(5);
+  right_motor.write(0.0);
+  right_motor.enable(true);
+  mraa::Gpio right_dir = mraa::Gpio(4);
+  right_dir.dir(mraa::DIR_OUT);
+  right_dir.write(0);
+
+  double speed = -1.0;
+  while (running) {
+    //std::cout << "Speed: " << "0.5" << std::endl;
+    setMotorSpeed(right_motor, right_dir, -0.5);
+    setMotorSpeed(left_motor, left_dir, 0.5);
+    sleep(4.0);
+
+    setMotorSpeed(right_motor, right_dir, 0.0);
+    setMotorSpeed(left_motor, left_dir, 0.0);
+    sleep(2.0);
+
+    setMotorSpeed(right_motor, right_dir, 0.5);
+    setMotorSpeed(left_motor, left_dir, -0.5);
+    sleep(4.0);
+
+    setMotorSpeed(right_motor, right_dir, 0.0);
+    setMotorSpeed(left_motor, left_dir, 0.0);
+    sleep(2.0);
+    /*speed += 0.1;
+    if (speed > 1.0) {
+      speed = -1.0;
+      // Let the motor spin down
+      setMotorSpeed(right_motor, right_dir, 0.0);
+      sleep(2.0);
+    }*/
+  }
+}
---
+++ test/ultrasonic.cpp
@@ -0,0 +1,81 @@
+#include <csignal>
+#include <iostream>
+#include <sys/time.h>
+
+#include "mraa.hpp"
+
+int running = 1;
+
+void sig_handler(int signo)
+{
+  if (signo == SIGINT) {
+    printf("closing spi nicely\n");
+    running = 0;
+  }
+}
+
+static double distance = -1.0;
+int counter = 0;
+
+void echo_handler(void* args) {
+  // Grab end time first, for accuracy
+  struct timeval end;
+  gettimeofday(&end, NULL);
+
+  mraa::Gpio* echo = (mraa::Gpio*)args;
+  static struct timeval start;
+  bool rising = echo->read() == 1;
+
+  std::cout << "Echo: " << echo->read() << std::endl;
+  std::cout << "Counter: " << counter << std::endl;
+
+  counter++;
+  /*if (rising) {
+    std::cout << "rising!" << std::endl;
+    gettimeofday(&start, NULL);
+  }
+  else {
+    int diffSec = end.tv_sec - start.tv_sec;
+    int diffUSec = end.tv_usec - start.tv_usec;
+    double diffTime = (double)diffSec + 0.000001*diffUSec;
+
+    // Speed of sound conversion: 340m/s * 0.5 (round trip)
+    distance = diffTime * 170.0;
+
+    std::cout << "Start Time: " << start.tv_sec << std::endl;
+    std::cout << "End Time: " << end.tv_sec << std::endl;
+    std::cout << "Diff sec: " << diffSec << std::endl;
+    std::cout << "Diff usec: " << diffUSec << std::endl;
+    std::cout << "Diff time: " << diffTime << std::endl;
+    std::cout << "Distance: " <<  distance << "m" << std::endl;
+    
+  }*/
+}
+
+int main() {
+  // Handle Ctrl-C quit
+  signal(SIGINT, sig_handler);
+
+  mraa::Gpio* trig = new mraa::Gpio(2);
+  trig->dir(mraa::DIR_OUT);
+  mraa::Gpio* echo = new mraa::Gpio(4);
+  echo->dir(mraa::DIR_IN);
+  // Set the echo handlers to receive rising or falling edges of the
+  // echo pulse
+  echo->isr(mraa::EDGE_RISING, echo_handler, echo);
+
+  mraa::Gpio* led = new mraa::Gpio(13);
+  led->dir(mraa::DIR_OUT);
+
+  while (running) {
+    // 20us trigger pulse (must be at least 10us)
+    trig->write(1);
+    led->write(1);
+    usleep(20);
+    trig->write(0);
+    led->write(0);
+
+    // Must pause at least 60ms between measurements
+    usleep(500000.0);
+  }
+}
